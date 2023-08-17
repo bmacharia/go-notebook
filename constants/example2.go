98cb972fa0b1d482ec0993994aab87bc1b063ca2
Babu Macharia <babu.macharia@gmail.com>
2023-08-17 14:34:15 -0700

    sample prgram using constants

---
+++ constants/example2.go
@@ -0,0 +1,20 @@
+//Sample program to show how contants do have a parallel type system
+
+package main
+
+import "fmt"
+
+const (
+	//Max integer value on 64 bit architecture
+	manInt = 9223372036854775807
+
+	//Much larger value than int64
+	bigger = 9223372036854775808543522345
+
+	//WILL NOT COMPILE
+	//biggerInt int64 = 9223372036854775808543522345
+)
+
+func main() {
+	fmt.Println("Will Compile")
+}
